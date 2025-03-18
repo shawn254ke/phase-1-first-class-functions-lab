3f740d5f9c760d2d3dd705343224f583ef7ff74f
shawnotieno <otienoshawn41@gmail.com>
2025-03-18 11:13:00 +0300

    assingment submission

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
-// Code your solution in this file!
+// Return first two drivers from an array
+const returnFirstTwoDrivers = function (drivers) {
+    return drivers.slice(0, 2);
+  };
+  
+  // Return last two drivers from an array
+  const returnLastTwoDrivers = function (drivers) {
+    return drivers.slice(-2);
+  };
+  
+  // Array containing the two functions
+  const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+  
+  // Function that creates a fare multiplier
+  const createFareMultiplier = function (multiplier) {
+    return function (fare) {
+      return fare * multiplier;
+    };
+  };
+  
+  // Fare doubler function
+  const fareDoubler = createFareMultiplier(2);
+  
+  // Fare tripler function
+  const fareTripler = createFareMultiplier(3);
+  
+  // Function that selects different drivers based on the provided function
+  const selectDifferentDrivers = function (drivers, driverSelector) {
+    return driverSelector(drivers);
+  };
+  
+  
+  
